05463c63987b75ea4ff684065f6f1787412bdf8b
Lucas Luna <112989163+LuccasLuna@users.noreply.github.com>
2023-10-02 16:42:11 -0300

    Add files via upload

--- assets/js/cadastro.js
+++ assets/js/cadastro.js
@@ -1,48 +1,69 @@
 
 const listaSangue = document.querySelector('.ul-sangue');
-const liSangue = document.querySelector('.li-sangue');
+
+
+listaSangue.addEventListener('click', (e) => {
+
+  if(e.target.tagName === 'INPUT') {
+    const input = e.target;
+
+    const allLabels = document.querySelectorAll('.ul-sangue label');
+
+    //remove o estido de todos os labels
+    allLabels.forEach(label => {
+      label.classList.remove('clicado');
+    })
+
+    // adiciona o estilo ao label referente ao input clicado
+    const label = document.querySelector(`label[for="${input.id}"]`);
+    label.classList.add('clicado');
+  }
+});
 
 
 
+
+
+
+/*
 listaSangue.addEventListener('click', (e)=> {
   const className = e.target.getAttribute('id'); // ta pegando o id do input
   const input = document.querySelector(`#${className}`);
   const label = document.querySelector(`.${className}`); // ta criando uma variavel para a label  que tem a classe com o menos nome do id do input
 
 
-  input.classList.add('active')
-  // //console.log(e.target.getAttribute('id'));
-  if(!label.classList.contains('clicado')) {
-     label.classList.add('clicado');
-  } // else {
-  //   label.classList.remove('clicado');
-  // }
+if(!label.classList.contains('clicado')) {
+    label.classList.add('clicado');
+    input.classList.add('active');
+}
 
-
-  // if(e.target.getAttribute('id') !== input.getAttribute('id')){
-  //   label.classList.add('clicado');
-  // }
 
   
 });
 
-/*
-listaSangue.addEventListener('click', handleEvent);
-
-function handleEvent(e) {
-  const className = e.target.getAttribute('id'); // ta pegando o id do input
-
-  const input = document.querySelector(`#${className}`);
-
-  const label = document.querySelector(`.${className}`); // ta criando uma variavel para a label  que tem a classe com o menos nome do id do input
-
- input.addEventListener('onChange', ()=> { // tentando pegar o evento no input
-
-   if(!label.classList.contains('clicado')) {
-     label.classList.add('clicado');
-  } else {
-    label.classList.remove('clicado');
+  for(let chave in input) {
+    if(!chave.classList('active')){
+      label.classList.remove('clicado');
+    } else {
+      label.classList.add('clicado');
+    }
   }
- })
-}
-*/
+ */  
+
+
+
+
+  /*
+  // //console.log(e.target.getAttribute('id'));
+  if(!label.classList.contains('clicado')) {
+    label.classList.add('clicado');
+  } // else {
+    //   label.classList.remove('clicado');
+    // }
+    
+    
+    // if(e.target.getAttribute('id') !== input.getAttribute('id')){
+      //   label.classList.add('clicado');
+      // }
+  */
+     
--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,3 @@ const userIcon = document.querySelector('#user-icon');
 
 listaIcon.addEventListener("mouseover", () => icon.src = `https://img.icons8.com/pastel-glyph/50/ffffff/vampire.png`);
 listaIcon.addEventListener("mouseout", () => icon.src = `https://img.icons8.com/pastel-glyph/50/FF0046/vampire.png`);
-
-
-//userIcon.addEventListener("mouseover", () => userIcon.classList.replace('fa-solid', 'fa-regular'));
-//userIcon.addEventListener("mouseout", () => userIcon.classList.replace('fa-regular', 'fa-solid'));
