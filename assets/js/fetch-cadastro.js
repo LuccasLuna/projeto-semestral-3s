d3ffd7c1e3ab7b8e2045e62d143a7f81875bd901
Lucas Luna <112989163+LuccasLuna@users.noreply.github.com>
2023-10-27 14:21:12 -0300

    Add files via upload

--- assets/js/fetch-cadastro.js
+++ assets/js/fetch-cadastro.js
@@ -5,7 +5,6 @@ function checaSelect() {
   const value = select.options[select.selectedIndex].value;
 
   if(value !== '0') {
-    console.log(value);
     alert('desculpe, mas você não pode doar sangue'); 
     return window.location.href = '../index.html'; // retorna pro index
     // return  window.history.back();
@@ -16,15 +15,16 @@ function checaSelect() {
 form.addEventListener('submit', async (e) =>  {
   e.preventDefault();
   checaSelect(); //chega se o usuário possui alguma doença
-
+   
   const valorPrompt = window.prompt(`Ao enviar você concorda com nossos termos, onde sua vida se torna propriedade exclusiva, irremediável e irrevogável do instituto Zé vampirinho. Na qual é resguardado o direito do possuinte de uso de sua propriedade como bem intender. Se você concorda digite "Sim", caso contrario, digite "Não"`);
 
   if (valorPrompt.toLowerCase() !== 'sim') {
     alert('Você não aceitou os termos, seu cadastro não foi efetuado!');
     return window.location.href = '../index.html';
   } else {
-    
+
      // tbm poderia usar o dataForm
+    /*
     const data = {
       email: document.querySelector('#email').value,
       senha: document.querySelector('#senha').value,
@@ -45,6 +45,7 @@ form.addEventListener('submit', async (e) =>  {
       complemento: document.querySelector('#complemento').value,
       doencas: document.querySelector('#select-doencas').value
     }
+    */
 
     try {
       const responsta = await fetch("", {
@@ -53,9 +54,28 @@ form.addEventListener('submit', async (e) =>  {
           "Content-Type": "application/json",
           "Accept": "application/json"
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify({
+          email: document.querySelector('#email').value,
+          senha: document.querySelector('#senha').value,
+          nome: document.querySelector('#nome').value,
+          dtNascimento: document.querySelector('#data-nasc').value,
+          cpf: document.querySelector('#cpf').value,
+          telefone: document.querySelector('#telefone').value,
+          altura: document.querySelector('#altura').value,
+          peso: document.querySelector('#peso').value,
+          sexo: document.querySelector('input[name="sexo"]:checked').value,
+          sangue: document.querySelector('input[name="sangue"]:checked').value,
+          rua: document.querySelector('#rua').value,
+          bairro: document.querySelector('#bairro').value,
+          municipio: document.querySelector('#municipio').value,
+          uf: document.querySelector('#uf').value,
+          numero: document.querySelector('#numero').value,
+          cep: document.querySelector('#cep').value,
+          complemento: document.querySelector('#complemento').value,
+          doencas: document.querySelector('#select-doencas').value
+        })
       });
-
+  
       if (!responsta.ok) {
         console.log('Erro na requisição.');
       } else {
@@ -67,6 +87,5 @@ form.addEventListener('submit', async (e) =>  {
     }
   }
 
- 
 });
   
